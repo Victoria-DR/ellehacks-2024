5c85da57eddbaa24cc199c7d72b04b239cfef9b7
Victoria <68347113+Victoria-DR@users.noreply.github.com>
2024-02-16 22:55:10 -0500

    Use next-pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+import withPWA from "next-pwa";
+import runtimeCaching from "next-pwa/cache.js";
+
+const nextConfig = withPWA({
+  dest: "public",
+  runtimeCaching,
+});
 
 export default nextConfig;
