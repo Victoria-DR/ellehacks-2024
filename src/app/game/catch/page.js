86993cff59b0b3bf640c208920e449aba579df0c
Victoria <68347113+Victoria-DR@users.noreply.github.com>
2024-02-17 02:36:37 -0500

    Get current location of user

--- src/app/game/catch/page.js
+++ src/app/game/catch/page.js
@@ -1,3 +1,31 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function Page() {
+  const [coordinates, setCoordinates] = useState({});
+
+  const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+  };
+
+  const success = (pos) => {
+    setCoordinates(pos.coords);
+  };
+
+  const error = (err) => {
+    console.log(err);
+  };
+
+  useEffect(() => {
+    const fetchCoordinates = async () => {
+      await navigator.geolocation.getCurrentPosition(success, error, options);
+    };
+
+    fetchCoordinates();
+    console.log("Location: ", coordinates);
+  });
   return <p>Catch Page</p>;
 }
