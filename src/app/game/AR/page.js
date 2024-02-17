6d91697f90dd02382e7cad0547d47abc6a35c616
amandakt <akytong70@gmail.com>
2024-02-17 09:31:55 -0500

    testing ar.js with react again

--- src/app/game/AR/page.js
+++ src/app/game/AR/page.js
@@ -1,26 +1,100 @@
 "use client";
-// pages/index.js (or HomePage.js)
-import React, { useEffect } from "react";
-import ARScene from "../../components/ARScene";
+// // pages/index.js (or HomePage.js)
+// import React, { useEffect } from "react";
+// import ARScene from "../../components/ARScene";
 
-const HomePage = () => {
+// const HomePage = () => {
+//   useEffect(() => {
+//     // Dynamically import ARComponent and load necessary scripts
+//     import("../../components/ARComponent").then((ARComponentModule) => {
+//       const ARComponent = ARComponentModule.default;
+//       // Load additional scripts here if needed
+//     });
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Your Next.js App</h1>
+//       <div id="ar-component-container">
+//         {/* Placeholder for ARComponent */}
+//         <ARScene />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default HomePage;
+
+// pages/ar.js
+// ARPage.js
+
+import Head from "next/head";
+import { useEffect } from "react";
+import { Helmet } from "react-helmet";
+
+const ARPage = () => {
   useEffect(() => {
-    // Dynamically import ARComponent and load necessary scripts
-    import("../../components/ARComponent").then((ARComponentModule) => {
-      const ARComponent = ARComponentModule.default;
-      // Load additional scripts here if needed
-    });
+    // Perform any client-side logic here
+    // For example, you may want to initialize AR-related functionality
   }, []);
 
   return (
-    <div>
-      <h1>Your Next.js App</h1>
-      <div id="ar-component-container">
-        {/* Placeholder for ARComponent */}
-        <ARScene />
+    <>
+      <Head>
+        <title>AR.js A-Frame Location-based</title>
+      </Head>
+      <Helmet>
+        <script src="https://aframe.io/releases/1.3.0/aframe.min.js"></script>
+        <script
+          type="text/javascript"
+          src="https://raw.githack.com/AR-js-org/AR.js/master/three.js/build/ar-threex-location-only.js"
+        ></script>
+        <script
+          type="text/javascript"
+          src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js"
+        ></script>
+      </Helmet>
+
+      <div>
+        {/* Your A-Frame content */}
+        <a-scene
+          vr-mode-ui="enabled: false"
+          arjs="sourceType: webcam; videoTexture: true; debugUIEnabled: false"
+          renderer="antialias: true; alpha: true"
+        >
+          <a-camera gps-projected-camera="gpsMinDistance: 5"></a-camera>
+          <a-entity
+            material="color: red"
+            geometry="primitive: box"
+            gps-projected-entity-place="latitude: 79.50627305573806; longitude: 43.77250049156359"
+            position="40 -15 0"
+            scale="10 10 10"
+          ></a-entity>
+        </a-scene>
+
+        <a-scene
+          vr-mode-ui="enabled: false"
+          arjs="sourceType: webcam; videoTexture: true; debugUIEnabled: false"
+          renderer="antialias: true; alpha: true"
+        >
+          <a-camera gps-projected-camera="gpsMinDistance: 5"></a-camera>
+          <a-entity
+            material="color: yellow"
+            geometry="primitive: cylinder"
+            gps-projected-entity-place="latitude: 79.50627305573806; longitude: 43.77250049156359"
+            position="-30 -15 0"
+            scale="10 10 10"
+          ></a-entity>
+          <a-text
+            value="Subject Title"
+            position="-31 -15 0"
+            look-at="[gps-camera]"
+            scale="5 5 5"
+          ></a-text>
+        </a-scene>
       </div>
-    </div>
+    </>
   );
 };
 
-export default HomePage;
+export default ARPage;
--- src/app/game/catch/page.js
+++ src/app/game/catch/page.js
@@ -1,13 +1,31 @@
-import React from "react";
-import WebcamComponent from "../../components/WebcamComponent";
+// import React from "react";
+// import WebcamComponent from "../../components/WebcamComponent";
 
-const Page = () => {
+// const Page = () => {
+//   return (
+//     <div>
+//       <h1>Webcam App</h1>
+//       <WebcamComponent />
+//     </div>
+//   );
+// };
+
+// export default Page;
+
+// pages/index.js
+
+import ARPage from "../../game/AR/page.js";
+
+const HomePage = () => {
   return (
     <div>
-      <h1>Webcam App</h1>
-      <WebcamComponent />
+      {/* Your other Next.js page content */}
+      <h1>Welcome to My Next.js App</h1>
+
+      {/* Include the ARPage component */}
+      <ARPage />
     </div>
   );
 };
 
-export default Page;
+export default HomePage;
