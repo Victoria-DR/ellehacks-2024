3450f7ceca68444ca9f7809b95e9801c56b323eb
Victoria <68347113+Victoria-DR@users.noreply.github.com>
2024-02-18 02:08:14 -0500

    Add gtlf model

--- src/app/game/catch/page.js
+++ src/app/game/catch/page.js
@@ -71,22 +71,20 @@ export default function Page() {
             renderer="antialias: true; alpha: true"
           >
             <a-camera gps-projected-camera="gpsMinDistance: 5"></a-camera>
-            {!entityQuiz && (
-              <a-entity
-                material="color: red"
-                geometry="primitive: box"
-                gps-projected-entity-place="latitude: 79.50627305573806; longitude: 43.77250049156359"
-                position="40 -15 0"
-                scale="10 10 10"
-              ></a-entity>
-            )}
+
+            <a-assets>
+              <a-asset-item
+                id="blazing-star"
+                src="../../assets/gltf/wildflower_bouquet/scene.gltf"
+              ></a-asset-item>
+            </a-assets>
 
             {!entityQuiz && (
               <a-entity
-                material="color: green"
-                geometry="primitive: cylinder"
+                material="color: red"
+                gltf-model="#blazing-star"
                 gps-projected-entity-place="latitude: 79.50627305573806; longitude: 43.77250049156359"
-                position="-30 -15 0"
+                position="40 -15 0"
                 scale="10 10 10"
               ></a-entity>
             )}
