eef9a178f0bb9810e1baeb40f6a3e53a817125b1
Victoria <68347113+Victoria-DR@users.noreply.github.com>
2024-02-18 06:41:26 -0500

    Add Auth0 authentication

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,15 @@
 "use client";
 
 import { useEffect } from "react";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
-import BadgeButton from "./components/BadgeButton";
 
 export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    router.push("/game/catch");
+    router.push("/api/auth/login");
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return (
-    <main style={{ width: "100vw", backgroundColor: "#fff" }}>
-      <Link href="/game/badges">
-        <BadgeButton />
-      </Link>
-    </main>
-  );
+  return <main style={{ width: "100vw", backgroundColor: "#fff" }}></main>;
 }
