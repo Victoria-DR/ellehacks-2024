19d6265715076d341f556fce1f129b45cbb6084b
Victoria <68347113+Victoria-DR@users.noreply.github.com>
2024-02-18 02:14:24 -0500

    Fix environment variable config

--- src/lib/db/mongodbClient.js
+++ src/lib/db/mongodbClient.js
@@ -1,5 +1,9 @@
 import { MongoClient } from "mongodb";
 
-const client = new MongoClient(process.env.NEXT_PUBLIC_MONGODB_URI);
+const client = new MongoClient(
+  process.env.NODE_ENV === "development"
+    ? process.env.NEXT_PUBLIC_MONGODB_URI
+    : process.env.MONGODB_URI
+);
 
 export default client;
